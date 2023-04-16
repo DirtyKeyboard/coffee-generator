5f8f7cb3484e2f7917756ee0adcc3f156aa9ade2
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-16 02:05:10 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -4,26 +4,8 @@ import {useState} from 'react'
 
 function App() {
   const [gen, setGen] = useState("Generate a Coffee using this Button!")
-  const [cat, setCat] = useState("All")
-  const HOTCOFFEE = [
-    "Americano",
-    "Veranda Blend",
-    "Pike Place",
-    "Dark Roast",
-    "Cafe Misto",
-    "Cappuccino",
-    "Espresso",
-    "Espresso Con Panna",
-    "Flat White",
-    "Honey Almondmilk Flatwhite",
-    "Latte",
-    "Cinnamon Dolce Latte",
-    "Blonde Vanilla Latte",
-    "Caramel Macchiato",
-    "Espresso Macchiato",
-    "Mocha",
-    "White Chocolate Mocha"
-  ]
+  const [cat, setCat] = useState(1)
+
   
   const HOTTEA = [
     "Chai Tea",
@@ -68,6 +50,26 @@ function App() {
     "Vanilla Bean",
     "White Chocolate Creme"
   ]
+
+  const HOTCOFFEE = [
+    "Americano",
+    "Veranda Blend",
+    "Pike Place",
+    "Dark Roast",
+    "Cafe Misto",
+    "Cappuccino",
+    "Espresso",
+    "Espresso Con Panna",
+    "Flat White",
+    "Honey Almondmilk Flatwhite",
+    "Latte",
+    "Cinnamon Dolce Latte",
+    "Blonde Vanilla Latte",
+    "Caramel Macchiato",
+    "Espresso Macchiato",
+    "Mocha",
+    "White Chocolate Mocha"
+  ]
   
   const COLDCOFFEE = [
     "Chocolate Creme Cold Brew",
@@ -88,7 +90,7 @@ function App() {
     "Iced Flat White",
     "Honey Almondmilk Flat White",
     "Latte",
-    "Cinnamon Dulce Latte",
+    "Cinnamon Dolce Latte",
     "Blonde Vanilla Latte",
     "Caramel Macchiato",
     "White Chocolate Mocha",
@@ -152,18 +154,38 @@ function App() {
     "Chocolate Malt Powder",
     "Vanilla Bean Powder"
   ]
+
+  const ALL = [...HOTCOFFEE, ...HOTTEA , ...HOTDRINK, ...FRAPS, ...COLDCOFFEE, ...ICEDTEA, ...COLD]
+
   function generateCoffee() {
+    let catStr = ""
+    switch (cat) {
+      case 1: catStr = 'Hot Coffee'; break;
+      case 2: catStr = 'Hot Tea'; break;
+      case 3: catStr = 'Hot Drink'; break;
+      case 4: catStr = 'Frappuccino'; break;
+      case 5: catStr = 'Cold Coffee'; break;
+      case 6: catStr = 'Iced Tea'; break;
+      case 7: catStr = 'Cold Drink'; break;
+    }
+    let drink = ""
+    switch (cat) {
+      case 1: drink = HOTCOFFEE[Math.floor(Math.random() * HOTCOFFEE.length)]; break;
+      case 2: drink = HOTTEA[Math.floor(Math.random() * HOTTEA.length)]; break;
+      case 3: drink = HOTDRINK[Math.floor(Math.random() * HOTDRINK.length)]; break;
+      case 4: drink = FRAPS[Math.floor(Math.random() * FRAPS.length)]; break;
+      case 5: drink = COLDCOFFEE[Math.floor(Math.random() * COLDCOFFEE.length)]; break;
+      case 6: drink = ICEDTEA[Math.floor(Math.random() * ICEDTEA.length)]; break;
+      case 7: drink = COLD[Math.floor(Math.random() * COLD.length)]; break;
+    }
 
-    //Generate Random Drink
-    //Generate Random Num of Toppings
-    /*
-    String Format:
-    Category: {cat}
-    Drink: {drink}
-    Toppings: {topping1}, {topping2}
-
-    */
-    alert(cat)
+    let toppings = []
+    for (let i = 0; i < Math.floor(Math.random() * 3)+2; i++) {
+      toppings.push(TOPPINGS[Math.floor(Math.random() * TOPPINGS.length)])
+    }
+    toppings[toppings.length-1] = `and ${toppings[toppings.length-1]}`
+    let toppingsStr = toppings.join(", ")
+    setGen(`Your ${catStr}: ${drink} with ${toppingsStr}`)
   }
   const options = [
     { key: 1, text: 'Hot Coffee', value: 1 },
@@ -185,165 +207,17 @@ function App() {
       <Label>Select Category</Label>
       <br />
       <br />
-      <Dropdown clearable options={options} selection onChange={(e, data) => {
+      <Dropdown value={cat} options={options} selection onChange={(e, data) => {
         if (typeof(data.value) === 'number')
           setCat(data.value)
         else 
-          setCat('All')
-        //setCat(`${e.target.value}`)
+          setCat(Math.floor(Math.random()*6)+1)
         }}/>
+        <br />
+        <br />
+        <Button inverted href="https://andrewhawileh.com" content="Link to My Website :)"/>
     </div>
   );
 }
 
 export default App;
-
-
-/* HOT COFFEE [
-  Americano
-  Veranda Blend
-  Pike Place
-  Dark Roast
-  Cafe Misto
-  Cappuccino
-  Espresso
-  Espresso Con Panna
-  Flat White
-  Honey Almondmilk Flatwhite
-  Latte
-  Cinnamon Dolce Latte
-  Blonde Vanilla Latte
-  Caramel Macchiato
-  Espresso Macchiato
-  Mocha
-  White Chocolate Mocha
-]
-
-HOT TEA [
-  Chai Tea
-  Chai Tea Latte
-  Earl Grey Tea
-  London Fog Tea Latte
-  Royal English Breakfast Tea
-  Royal English Breakfast Tea Latte
-  Emperors Clouds and Mist
-  Matcha Tea
-  Honey Citrus Mint Tea
-  Jade Citrus Mint Tea
-  Mint Majesty
-  Peach Tranquility
-]
-
-HOT DRINK [
-  Hot Chocolate
-  White Hot Chocolate
-  Caramel Apple Spice
-  Steamed Apple Juice
-  Vanilla Creme
-  Steamed Milk
-]
-
-FRAPS [
-  Mocha Cookie Crumble
-  Caramel Ribbon Crunch
-  Espresso
-  Coffee
-  Caffe Vanilla
-  Caramel
-  Mocha
-  White Mocha
-  Java Chip
-  Chocolate Cookie Crumble
-  Caramel Ribbon Crunch Creme
-  Strawberry Creme
-  Chai
-  Double Chocolaty Chip
-  Matcha
-  Vanilla Bean
-  White Chocolate Creme
-]
-
-COLD COFFEE [
-  Chocolate Creme Cold Brew
-  Salted Caramel Creme Cold Brew
-  Cold Brew
-  Cold Brew with Milk
-  Vanilla Sweet Creme Cold Brew
-  Nitro Cold Brew
-  Vanilla Sweet Creme Nitro
-  Iced Americano
-  Iced Coffee
-  Iced Coffee with Milk
-  Iced Espresso
-  Toasted Vanilla Shaken Espresso
-  Brown Sugar Oatmilk Shaken Espresso
-  Chocoalte Almondmilk Shaken Espresso
-  Shaken Espresso
-  Iced Flat White
-  Honey Almondmilk Flat White
-  Latte
-  Cinnamon Dulce Latte
-  Blonde Vanilla Latte
-  Caramel Macchiato
-  White Chocolate Mocha
-  Mocha
-]
-
-ICED TEA [
-  Black Tea
-  Black Tea Lemonade
-  Iced Chai Tea Latte
-  Peach Green Tea
-  Peach Green Tea Lemonade
-  Matcha Tea Latte
-  Green Tea
-  Green Tea Lemonade
-  Matcha Lemonade
-  Passion Tango Tea
-  Passion Tango Tea Lemonade
-]
-
-COLD [
-  Dragon Drink
-  Mango Dragonfruit Refresher
-  Mango Dragonfruit Lemonade
-  Strawberry Acai
-  Strawberry Acai Lemonade
-  Pink Drink
-  Pineapple Passionfruit
-  Pineapple Passionfruit Lemonade
-  Paradise Drink
-]
-
-TOPPINGS [
-  Mocha Sauce
-  Dark Caramel Sauce
-  White Mocha Sauce
-  Chai
-  Classic Syrup
-  Honey Blend
-  Liquid Cane Sugar
-  Brown Sugar Syrup
-  Caramel Syrup
-  Cinnamon Dolce Syrup
-  Hazelnut Syrup
-  Peppermint Syrup
-  Raspberry Syrup
-  Sugar-Free Vanilla Syrup
-  Toasted Vanilla Syrup
-  Vanilla Syrup
-  Toffee Nut Syrup
-  Chocolate Creme Cold Foam
-  Salted Caramel Creme Cold Foam
-  Vanilla Sweet Creme Foam
-  Cinnamon Powder
-  Caramel Drizzle
-  Mocha Drizzle
-  Caramel Crunch Topping
-  Cinnamon Dolce Sprinkes
-  Cookie Crumble Topping
-  Whipped Cream
-  Chocolate Malt Powder
-  Vanilla Bean Powder
-]
- */
